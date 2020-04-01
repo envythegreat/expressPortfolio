13c951149633a8337f4e46157cfdc42a449fa35e
Alouani Mohamed Habib <greed.envyme@gmail.com>
2020-04-01 17:38:08 -0400

    Adding comments

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
+//require express module
 const express = require('express');
 const router = express.Router();
+// require the json data needed
 const { projects } = require('../data/data.json');
 const { myinfo } = require('../data/profile.json');
-
+// Set your routes to render the index page
 router.get('/', (req, res) => {
-    res.render('index', { project: projects[0], allProject:projects, myinfo });
+    res.render('index', { 
+        // using the json data in index and layout pug pages
+        project: projects[0], 
+        allProject:projects, 
+        myinfo 
+    });
 })
+// exporting the router so we can use it in the app.js
 module.exports = router;
--- routes/project.js
+++ routes/project.js
@@ -1,10 +1,19 @@
+//require express module
 const express = require('express');
 const router = express.Router();
+// require the json data needed
 const { projects } = require('../data/data.json');
 const { myinfo } = require('../data/profile.json');
+// Set your routes to render a project page based on the parameter in the URL given
 router.get('/projects/:id', (req, res) => {
+    // capturing data from the url (:id) using the req.params method
     const {id} = req.params;
     // console.log(myinfo);
-    res.render('project', { project: projects[id], myinfo});
+    
+    res.render('project', { 
+        // using the json data in project pug pages
+        project: projects[id], 
+        myinfo
+    });
 })
 module.exports = router;
