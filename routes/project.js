57e4fd24d8e4aff25cd1c94ab5fef4676261294c
Alouani Mohamed Habib <greed.envyme@gmail.com>
2020-04-03 02:45:58 -0500

    last modification

--- routes/project.js
+++ routes/project.js
@@ -4,16 +4,24 @@ const router = express.Router();
 // require the json data needed
 const { projects } = require('../data/data.json');
 const { myinfo } = require('../data/profile.json');
+function findId(id) {
+    return projects.filter( projects => { return projects.id == id ? true : false } );
+}
 // Set your routes to render a project page based on the parameter in the URL given
-router.get('/projects/:id', (req, res) => {
+router.get('/projects/:id', (req, res, next) => {
     // capturing data from the url (:id) using the req.params method
     const {id} = req.params;
-    // console.log(myinfo);
-    
-    res.render('project', { 
+    const newID = findId(id);
+    if(newID == ""){
+        const err = new Error('Not found');
+        err.status = 404;
+        next(err)
+    }else{
+        res.render('project', { 
         // using the json data in project pug pages
         project: projects[id], 
         myinfo
-    });
+    });}
+    
 })
 module.exports = router;
